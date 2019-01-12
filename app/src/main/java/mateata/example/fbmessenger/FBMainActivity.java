5b317ce89ea64e6dc2a23a2e581941613cb9127e
mateata <kayanepsu@gmail.com>
2019-01-12 16:35:18 +0900

    초기

---
+++ app/src/main/java/mateata/example/fbmessenger/FBMainActivity.java
@@ -0,0 +1,13 @@
+package mateata.example.fbmessenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FBMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fbmain);
+    }
+}
