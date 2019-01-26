0daa80a61fea4c196647fb049343d63416af095e
mateata <kayanepsu@gmail.com>
2019-01-26 14:23:48 +0900

    Lombok 제거

--- app/src/main/java/mateata/example/fbmessenger/adapter/User.java
+++ app/src/main/java/mateata/example/fbmessenger/adapter/User.java
@@ -1,9 +1,46 @@
 package mateata.example.fbmessenger.adapter;
 
-import lombok.Data;
-
-@Data
 public class User {
     private String uid, email, name, profileUrl;
     private boolean selection;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+
+    public boolean isSelection() {
+        return selection;
+    }
+
+    public void setSelection(boolean selection) {
+        this.selection = selection;
+    }
 }
